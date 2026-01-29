082f902eece2de7cf25fe3563c8f76ac0bf5e5b7
GANDHIMATHY26 <224491705+GANDHIMATHY26@users.noreply.github.com>
2026-01-29 23:37:12 +0530

    Add IncreDecreOperators.java to demonstrate operators

---
+++ IncreDecreOperators.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Increment & Decrement Operator");
+		System.out.println("---------------------");
+		int num=5;
+		System.out.println(num++);
+		System.out.println(--num);
+		System.out.println(num++);
+		System.out.println(num--);
+		System.out.println(++num);
+	}
+}
