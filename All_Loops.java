3263629746738a67375346f7d225d67738461965
GANDHIMATHY26 <224491705+GANDHIMATHY26@users.noreply.github.com>
2026-02-01 00:43:14 +0530

    Add All_Loops.java to demonstrate loop types

---
+++ All_Loops.java
@@ -0,0 +1,28 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("------------");
+		System.out.println("For Loop");
+		System.out.println("------------");
+		for(int i=1;i<=10;i++){
+		    System.out.println(i);
+		}
+		System.out.println("------------");
+		System.out.println("While Loop");
+		System.out.println("------------");
+		int j=1;
+		while(j<=10){
+		    System.out.println(j);
+		    j++;
+		}
+		System.out.println("---------------");
+		System.out.println("Do While Loop");
+		System.out.println("---------------");
+		int num=1;
+		do{
+		    System.out.println(num);
+		    num++;
+		}while (num<=10);
+		
+	}
+}
