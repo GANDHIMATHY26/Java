a99c50d12d2ecf70c69e7b6bb83c9fa629da0a94
GANDHIMATHY26 <224491705+GANDHIMATHY26@users.noreply.github.com>
2026-02-06 20:43:07 +0530

    Add Welcome class with greeting functionality

---
+++ Greetings.java
@@ -0,0 +1,9 @@
+public class Welcome {
+    public static void welcome(String name) {
+        System.out.println("Hi " + name + ", Welcome to this page");
+    }
+    public static void main(String[] args) {
+        String name = "Gandhimathy";
+        welcome(name);
+    }
+}
